38d8d89052b6ba63e489bee057f6bc5da5a2cc5d
DavidTaapy <74765863+DavidTaapy@users.noreply.github.com>
2023-06-18 22:56:38 +0800

    Add MongoDB Realm Code

---
+++ realm code/cuisines.js
@@ -0,0 +1,8 @@
+// Function to get the different cuisines
+exports = async function(payload, response) {
+
+    const collection = context.services.get("mongodb-atlas").db("sample_restaurants").collection("restaurants");
+    const cuisines = await collection.distinct("cuisine");
+    
+    return cuisines;
+  };
